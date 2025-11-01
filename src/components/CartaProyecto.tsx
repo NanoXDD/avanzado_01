a6b7e2ae12e9cccc87cb908814951134ec530369
NanoXDD <nanopchg@gmail.com>
2025-11-01 10:09:51 -0400

    Agregacion de pokemon
    Agregue a capitan peru de pokemon

---
+++ src/components/CartaProyecto.tsx
@@ -0,0 +1,37 @@
+type Props ={
+    numero: number;
+    nombre: string;
+    tipo: string;
+    ataque: number;
+    defensa: number;
+    descripcion: string;
+    imagen: string;
+};
+
+function TarjetaDetalle({
+    ataque,
+    defensa,
+    descripcion,
+    imagen,
+    nombre,
+    numero,
+    tipo,
+}: Props) {
+    return(
+        <div>
+            <h3>
+            {nombre} (#{numero})
+            </h3>
+        <img src={imagen} alt={nombre} />
+        <p>Tipo: {tipo}</p>
+        <p>Ataque: {ataque}</p>
+        <p>Defensa: {defensa}</p>
+        <p>{descripcion}</p>
+        </div>
+    );
+}
+
+export default TarjetaDetalle;
+
+
+
